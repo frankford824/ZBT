4c1cf1b73f0c912084796f384bea44e2675e443c
frankford824 <frankford824@users.noreply.github.com>
2026-06-16 06:39:53 -0700

    Split frontend vendor chunks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,38 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: 'charts',
+              test: /[\\/]node_modules[\\/](echarts|echarts-for-react|zrender)[\\/]/,
+              priority: 30,
+              maxSize: 360_000,
+            },
+            {
+              name: 'antd',
+              test: /[\\/]node_modules[\\/]antd[\\/]/,
+              priority: 20,
+              maxSize: 360_000,
+            },
+            {
+              name: 'rc',
+              test: /[\\/]node_modules[\\/](@rc-component|rc-)[\\/]/,
+              priority: 15,
+              maxSize: 300_000,
+            },
+            {
+              name: 'editor',
+              test: /[\\/]node_modules[\\/](@tiptap|prosemirror-)[\\/]/,
+              priority: 10,
+              maxSize: 360_000,
+            },
+          ],
+        },
+      },
+    },
+  },
 })
